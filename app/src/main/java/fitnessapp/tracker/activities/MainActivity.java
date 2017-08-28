c670e675320ce019aee1dbe4b48974355bf82685
kraemerj <jan.kraemer@warema.de>
2017-08-28 14:48:02 +0200

    change MainActivity

--- app/src/main/java/fitnessapp/tracker/activities/MainActivity.java
+++ app/src/main/java/fitnessapp/tracker/activities/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -26,6 +30,7 @@ import java.util.List;
 
 import fitnessapp.tracker.R;
 import fitnessapp.tracker.adapters.ExerciseAdapter;
+import fitnessapp.tracker.adapters.MainPagerAdapter;
 import fitnessapp.tracker.database.DatabaseHelper;
 import fitnessapp.tracker.interfaces.IOnItemClickListener;
 import fitnessapp.tracker.models.Training;
@@ -33,10 +38,9 @@ import fitnessapp.tracker.models.Training;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private DatabaseHelper databaseHelper = null;
-    private RecyclerView recyclerView;
-    private LinearLayoutManager linearLayoutManager;
-    private ExerciseAdapter adapter;
+    private MainPagerAdapter mainPagerAdapter;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +48,21 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        initViewPager();
         setNavigationDrawerOnActivity(toolbar);
         setFloatinPointAction();
-        initRecyclerView();
-        callDatabaseForAllExercises();
+    }
+
+    private void initViewPager() {
+        String[] titles = new String[]{"Woche","Monat"};
+
+        mainPagerAdapter = new MainPagerAdapter(this, getSupportFragmentManager(),titles);
+        viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(mainPagerAdapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 
     private void setFloatinPointAction() {
@@ -129,48 +144,5 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (databaseHelper != null) {
-            OpenHelperManager.releaseHelper();
-            databaseHelper = null;
-        }
-    }
-
-    private DatabaseHelper getHelper() {
-        if (databaseHelper == null) {
-            databaseHelper =
-                    OpenHelperManager.getHelper(this, DatabaseHelper.class);
-        }
-        return databaseHelper;
-    }
-
-    private void initRecyclerView(){
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerview_main);
-        linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        adapter = new ExerciseAdapter(this, new IOnItemClickListener() {
-            @Override
-            public void onClick(int position) {
-                //open a DetailView for the Training with all Exercises and so on.
-            }
-        });
-        recyclerView.setAdapter(adapter);
-    }
-    private void callDatabaseForAllExercises() {
-        new Handler().post(new Runnable() {
-            @Override
-            public void run() {
-                try{
-                    List<Training> items = getHelper().getTrainingDao().queryForAll();
-                    adapter.addTrainingsToAdapter(items);
-                }catch (SQLException e){
-                    Log.e("SQL Exception", "Error on loading the trainings.",e);
-                }
-            }
-        });
     }
 }
