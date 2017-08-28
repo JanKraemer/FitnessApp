415d7c5f6fe3dfa3c520ede79ff669d9d330f3ea
kraemerj <jan.kraemer@warema.de>
2017-08-28 14:47:53 +0200

    add new classes for TabLayout on MainActivity

---
+++ app/src/main/java/fitnessapp/tracker/adapters/MainPagerAdapter.java
@@ -0,0 +1,10 @@
+package fitnessapp.tracker.adapters;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by kraemerj on 28.08.2017.
+ */
+
+public class MainPagerAdapter extends FragmentPagerAdapter {
+}
---
+++ app/src/main/java/fitnessapp/tracker/fragments/WeekFragment.java
@@ -0,0 +1,8 @@
+package fitnessapp.tracker.fragments;
+
+/**
+ * Created by kraemerj on 28.08.2017.
+ */
+
+public class WeekFragment {
+}
