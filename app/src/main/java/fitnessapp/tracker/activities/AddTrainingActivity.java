01e3f35ec3e7c3e5280770fb908c632362ab0904
kraemerj <jan.kraemer@warema.de>
2017-09-25 14:53:21 +0200

    refactoring code

--- app/src/main/java/fitnessapp/tracker/activities/AddTrainingActivity.java
+++ app/src/main/java/fitnessapp/tracker/activities/AddTrainingActivity.java
@@ -12,30 +12,28 @@ import fitnessapp.tracker.R;
 public class AddTrainingActivity extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_add_training );
-        ActionBar actionBar = getSupportActionBar();
-        if ( actionBar != null )
-        {
+        ActionBar actionBar = getSupportActionBar( );
+        if ( actionBar != null ) {
             actionBar.setTitle( R.string.newTraining );
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setDisplayShowHomeEnabled(true);
-            actionBar.setHomeAsUpIndicator(getResources().getDrawable(R.drawable.ic_close_white_24dp));
+            actionBar.setDisplayHomeAsUpEnabled( true );
+            actionBar.setDisplayShowHomeEnabled( true );
+            actionBar.setHomeAsUpIndicator( getResources( ).getDrawable( R.drawable.ic_close_white_24dp ) );
         }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+    public boolean onOptionsItemSelected( MenuItem item ) {
+        int id = item.getItemId( );
 
-        if (id == android.R.id.home) {
-            onBackPressed();
+        if ( id == android.R.id.home ) {
+            onBackPressed( );
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected( item );
     }
 }
