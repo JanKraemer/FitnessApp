8166506abec3e46a96fe9f48f4ef504aeb01b7e6
kraemerj <jan.kraemer@warema.de>
2017-09-27 08:25:01 +0200

    add check for user cancels the action

--- app/src/main/java/fitnessapp/tracker/activities/AddTrainingActivity.java
+++ app/src/main/java/fitnessapp/tracker/activities/AddTrainingActivity.java
@@ -1,7 +1,10 @@
 package fitnessapp.tracker.activities;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,15 +13,17 @@ import android.view.MenuItem;
 import android.view.View;
 
 import fitnessapp.tracker.R;
+import fitnessapp.tracker.models.Training;
 
 public class AddTrainingActivity extends AppCompatActivity {
 
+    private boolean closeFlag = false;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_add_training );
-        initActionbar();
+        initActionbar( );
     }
 
     private void initActionbar( ) {
@@ -36,12 +41,13 @@ public class AddTrainingActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected( MenuItem item ) {
         int id = item.getItemId( );
 
-        switch (id) {
+        switch ( id ) {
             case android.R.id.home:
                 onBackPressed( );
                 return true;
 
             case R.id.action_save:
+                saveTraining( );
                 return true;
 
             default:
@@ -49,10 +55,35 @@ public class AddTrainingActivity extends AppCompatActivity {
         }
     }
 
+    private void saveTraining( ) {
+
+    }
+
     @Override
     public boolean onCreateOptionsMenu( Menu menu ) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater( ).inflate( R.menu.menu_addtraining, menu );
         return true;
+    }
+
+    @Override
+    public void onBackPressed( ) {
+        AlertDialog dialog = new AlertDialog.Builder( this )
+                .setTitle( getString( R.string.discard_title ) )
+                .setPositiveButton( R.string.discard, new DialogInterface.OnClickListener( ) {
+                    @Override
+                    public void onClick( DialogInterface dialog, int which ) {
+                        finish();
+                    }
+                } )
+                .setNegativeButton( R.string.cancel, new DialogInterface.OnClickListener( ) {
+                    @Override
+                    public void onClick( DialogInterface dialog, int which ) {
+                        dialog.cancel();
+                    }
+                } )
+                .create( );
+
+        dialog.show( );
     }
 }
