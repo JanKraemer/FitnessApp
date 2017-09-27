472704c77dfa8ef3941c11f731c629024a573a97
kraemerj <jan.kraemer@warema.de>
2017-09-27 11:44:31 +0200

    implement SpinnerAdapter

--- app/src/main/java/fitnessapp/tracker/adapters/SpinnerAdapter.java
+++ app/src/main/java/fitnessapp/tracker/adapters/SpinnerAdapter.java
@@ -1,10 +1,50 @@
 package fitnessapp.tracker.adapters;
 
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-/**
- * Created by kraemerj on 27.09.2017.
- */
+import java.util.ArrayList;
 
-public class SpinnerAdapter extends ArrayAdapter {
+import de.hdodenhof.circleimageview.CircleImageView;
+import fitnessapp.tracker.R;
+import fitnessapp.tracker.models.SpinnerItem;
+
+public class SpinnerAdapter extends ArrayAdapter< SpinnerItem > {
+
+    private ArrayList< SpinnerItem > items;
+
+    public SpinnerAdapter( Activity activity, int textview, ArrayList< SpinnerItem > items ) {
+        super( activity, R.layout.spinner_layout, textview, items );
+        this.items = items;
+    }
+
+    @Override
+    public View getDropDownView( int position, View convertView, @NonNull ViewGroup parent ) {
+        return getView( position, convertView, parent );
+    }
+
+    @Override
+    public SpinnerItem getItem( int position ) {
+        return items.get( position );
+    }
+
+    @NonNull
+    @Override
+    public View getView( int position, View v, @NonNull ViewGroup parent ) {
+        View view = super.getView( position,v,parent );
+        setValues( view, items.get( position ) );
+        return view;
+    }
+
+    private void setValues( View view, SpinnerItem item )
+    {
+        TextView lbl = ( TextView ) view.findViewById( R.id.spinner_text );
+        lbl.setText( item.getText( ) );
+        CircleImageView image = ( CircleImageView ) view.findViewById( R.id.spinner_image );
+        image.setImageResource( item.getImageId( ) );
+    }
 }
